cb7397811d3265606b1e27ddd2b11b3b091dc18a
turulko-oleksandr <sashaturulko@i.ua>
2025-09-22 12:52:01 +0300

    feat(books): create and migrate book model in db

--- library_service_api/admin.py
+++ library_service_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from library_service_api.models import Book
+from library_service_auth.models import Customer
+
 # Register your models here.
+admin.site.register(Book)
--- library_service_api/models.py
+++ library_service_api/models.py
@@ -1,3 +1,23 @@
+from email.policy import default
+from random import choices
+
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    daily_fee = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    inventory = models.PositiveIntegerField()
+    COVER_CHOICES = (
+        (1, 'HARD'),
+        (0, 'SOFT')
+    )
+    cover = models.IntegerField(
+        choices=COVER_CHOICES,
+        default=0
+    )
+
