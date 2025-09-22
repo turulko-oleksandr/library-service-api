d3577644698613d79b6dadcd91acc36c0c6caea0
turulko-oleksandr <sashaturulko@i.ua>
2025-09-22 12:20:41 +0300

    init: create connected projects setup

---
+++ library_service_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LibraryServiceApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'library_service_api'
---
+++ library_service_auth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LibraryServiceAuthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'library_service_auth'
